c4db4b901d97badee382ac7ceefbc374ecebdee4
FrankdaSilva-arch <fsa.ped22@gmail.com>
2025-04-05 12:13:46 -0400

    Atualizando wsgi.py para usar configurações do PythonAnywhere

--- config/wsgi.py
+++ config/wsgi.py
@@ -2,6 +2,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # Onde estiver:
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'config.settings_pythonanywhere')
 
 application = get_wsgi_application()
