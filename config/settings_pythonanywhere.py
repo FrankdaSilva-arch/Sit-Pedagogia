3963555466d7655d69dab87abc74b44dfcb95d3a
FrankdaSilva-arch <fsa.ped22@gmail.com>
2025-04-05 12:17:45 -0400

    Atualizando configurações do PythonAnywhere para não herdar do arquivo base

--- config/settings_pythonanywhere.py
+++ config/settings_pythonanywhere.py
@@ -1,20 +1,20 @@
-from .settings import *
+import os
+from pathlib import Path
 
-# Desativar modo debug em produção
+# Defina BASE_DIR no início do arquivo
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Configurações básicas
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY', 'django-insecure-1234567890-sua-chave-secreta-aqui')
 DEBUG = False
-
-# Configurar hosts permitidos
 ALLOWED_HOSTS = ['*']
-
-# Desativar logging completamente
-LOGGING_CONFIG = None
-LOGGING = {}
 
 # Configurações de banco de dados MySQL
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'FRANKPED2025$default',  # Nome do banco de dados existente
+        'NAME': 'FRANKPED2025$default',
         'USER': 'FRANKPED2025',
         'PASSWORD': os.environ.get('DB_PASSWORD', 'sua_senha'),
         'HOST': 'FRANKPED2025.mysql.pythonanywhere-services.com',
@@ -27,7 +27,12 @@ DATABASES = {
 }
 
 # Configurações de arquivos estáticos e mídia
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 # Configurações de segurança
@@ -37,3 +42,72 @@ CSRF_COOKIE_SECURE = True
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 X_FRAME_OPTIONS = 'DENY'
+
+# Configurações de idioma e fuso horário
+LANGUAGE_CODE = 'pt-br'
+TIME_ZONE = 'America/Sao_Paulo'
+USE_I18N = True
+USE_TZ = True
+
+# Configurações de login
+LOGIN_URL = '/admin/login/'
+LOGIN_REDIRECT_URL = '/admin/'
+LOGOUT_REDIRECT_URL = '/'
+
+# Configurações de sessão
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
+SESSION_COOKIE_AGE = 1209600  # 2 semanas em segundos
+SESSION_SAVE_EVERY_REQUEST = True
+
+# Configurações do Admin
+ADMIN_SITE_HEADER = "Administração do Sistema"
+ADMIN_SITE_TITLE = "Portal de Administração"
+ADMIN_INDEX_TITLE = "Bem-vindo ao Portal de Administração"
+
+# Configurações do Admin
+ADMIN_SITE_CSS = {
+    'all': ('admin/css/custom_admin.css',),
+}
+
+# Configurações de aplicativos
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'loja',
+]
+
+# Configurações de middleware
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# Configurações de templates
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+# Configurações de URLs
+ROOT_URLCONF = 'config.urls'
+WSGI_APPLICATION = 'config.wsgi.application'
